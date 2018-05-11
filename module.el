ed6e4cdde103518d024e1392f5e01b50f0848e54
Tyler L <tslimkemann42@gmail.com>
2018-05-10 17:40:59 -0700

    oops, need to require cl-lib

--- module.el
+++ module.el
@@ -1,3 +1,5 @@
+(require 'cl-lib)
+
 (defun list-functions () (cl-loop for s being the symbols if (fboundp s) collect (symbol-name s)))
 (defun list-symbols () (cl-loop for s being the symbols if (boundp s) collect (symbol-name s)))
 
