35daa7ed886ae7eeec172edff775e89453960b0e
Tyler L <tslimkemann42@gmail.com>
2018-05-10 21:56:05 -0700

    made lua-emacs.c easier to read

--- lua-emacs.c
+++ lua-emacs.c
@@ -10,44 +10,80 @@
 
 #include <stdlib.h>
 
-lua_State *lua_emacs_init(emacs_env *env) {
+static void compile_elisp_table(lua_State *L) {
+    //NOTE: THIS FUNCTION ASSUMES THE TOP OF THE STACK IS THE ELISP TABLE
+    lua_getglobal(L, "_G");
+    lua_insert(L, -2);
+    lua_pushstring(L, "elisp");
+    lua_insert(L, -2);
+    lua_settable(L, -3);
+}
+static void create_elisp_functions_table(lua_State *L, emacs_env *env) {
+    //NOTE: THIS FUNCTION ASSUMES THE TOP OF THE STACK IS THE TABLE WHICH WILL BE THE
+    //ELISP GLOBAL TABLE. IF IT IS NOT, THIS FUNCTION WILL NOT WORK.
     //first, get the list of functions from emacs
     //NOTE: this assumes that list-functions and list-symbols are defined already
     emacs_value function_list = slow_arbitrary_funcall(env, "list-functions", 0);
     intmax_t function_list_length = env->extract_integer(env, slow_arbitrary_funcall(env, "length", 1, function_list));
 
-    lua_State *L = lua_open();
-    luaL_openlibs(L);
-    
-    //the global table, at the bottom of the stack until the end
-    lua_getglobal(L, "_G");
-    
-        //the elisp table
-        lua_newtable(L);
-        //the elisp function table
-            lua_newtable(L);
-            for (intmax_t function_index = 0; function_index < function_list_length; function_index++) {
-                char *function_name_buf;
-                ptrdiff_t function_name_len;
-                emacs_value function_value = nth_in_list(env, function_list, function_index);
-                env->copy_string_contents(env, function_value, NULL, &function_name_len);
-                function_name_buf = malloc(function_name_len);
-                env->copy_string_contents(env, function_value, function_name_buf, &function_name_len);
-                lua_pushstring(L, function_name_buf);
-                free(function_name_buf);
-                lua_pushstring(L, "todo");
-                lua_settable(L, -3);
-            }
-            //add the function table to the elisp table
-            lua_pushstring(L, "functions");
-            lua_insert(L, -2);
-            lua_settable(L, -3);
-        //the elisp symbol table
-    
-    //add the elisp table to the global table
-    lua_pushstring(L, "elisp");
+    lua_newtable(L);
+    for (intmax_t function_index = 0; function_index < function_list_length; function_index++) {
+        char *function_name_buf;
+        ptrdiff_t function_name_len;
+        emacs_value function_value = nth_in_list(env, function_list, function_index);
+        env->copy_string_contents(env, function_value, NULL, &function_name_len);
+        function_name_buf = malloc(function_name_len);
+        env->copy_string_contents(env, function_value, function_name_buf, &function_name_len);
+        lua_pushstring(L, function_name_buf);
+        free(function_name_buf);
+        lua_pushstring(L, "TODO");
+        lua_settable(L, -3);
+    }
+    //add the function table to the elisp table
+    lua_pushstring(L, "functions");
     lua_insert(L, -2);
     lua_settable(L, -3);
+}
+
+static void create_elisp_symbols_table(lua_State *L, emacs_env *env) {
+    //NOTE: THIS FUNCTION ASSUMES THE TOP OF THE STACK IS THE TABLE WHICH WILL BE THE
+    //ELISP GLOBAL TABLE. IF IT IS NOT, THIS FUNCTION WILL NOT WORK.
+    //first, get the list of symbols from emacs
+    //NOTE: this assumes that list-functions and list-symbols are defined already
+    emacs_value symbol_list = slow_arbitrary_funcall(env, "list-symbols", 0);
+    intmax_t symbol_list_length = env->extract_integer(env, slow_arbitrary_funcall(env, "length", 1, symbol_list));
+
+    lua_newtable(L);
+    for (intmax_t symbol_index = 0; symbol_index < symbol_list_length; symbol_index++) {
+        char *symbol_name_buf;
+        ptrdiff_t symbol_name_len;
+        emacs_value symbol_value = nth_in_list(env, symbol_list, symbol_index);
+        env->copy_string_contents(env, symbol_value, NULL, &symbol_name_len);
+        symbol_name_buf = malloc(symbol_name_len);
+        env->copy_string_contents(env, symbol_value, symbol_name_buf, &symbol_name_len);
+        lua_pushstring(L, symbol_name_buf);
+        free(symbol_name_buf);
+        lua_pushstring(L, "TODO");
+        lua_settable(L, -3);
+    }
+    //add the symbol table to the elisp table
+    lua_pushstring(L, "symbols");
+    lua_insert(L, -2);
+    lua_settable(L, -3);
+}
+
+lua_State *lua_emacs_init(emacs_env *env) {
+    lua_State *L = lua_open();
+    luaL_openlibs(L);
+        
+    //the elisp table
+    lua_newtable(L);
+    //the elisp function table
+    create_elisp_functions_table(L, env);
+    //the elisp symbol table
+    create_elisp_symbols_table(L, env);
+    //add the elisp table to the global table
+    compile_elisp_table(L);
     
     return L;
 }
